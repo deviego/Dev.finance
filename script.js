e811202f2de44417730de74ddd8fde2a511f6635
deviego <Diegodomingues266@gmail.com>
2022-01-26 23:22:54 -0300

    criação do sistema que vai adicionar os data-table

--- script.js
+++ script.js
@@ -9,3 +9,75 @@ const Modal= {
         .classList.remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        descripiton: 'Luz',
+        amount: -50000,
+        date: '23/01/2022'
+    },
+    {
+        id: 2,
+        descripiton: 'Web',
+        amount: 500000,
+        date: '07/01/2022'
+    },
+    {
+        id: 3,
+        descripiton: 'internet',
+        amount: -100,
+        date: '07/01/2022' 
+    },
+]
+
+const transaction = {
+    incomes() {
+        //somar entrada 
+    },
+    expenses(){
+        //somar as saidas 
+    },
+    total(){
+        //somar o talal 
+    }
+
+}
+
+const DOM = {
+    TransactionContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.TransactionContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = 
+        `
+            <td class="description ">${transaction.descripiton}</td>
+            <td class="${CSSclass} ">${transaction.amount}</td>
+            <td class="date ">${transaction.date}</td>
+            <td><img src="assets/minus.svg" alt=""></td>
+         ` 
+         
+         return html
+    }
+       
+}
+
+DOM.addTransaction(transactions[1])
+
+const Utils {
+    formatCurrency(value) {
+        let signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
